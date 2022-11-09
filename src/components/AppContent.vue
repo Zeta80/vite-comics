34d17287e87e4397c5687b5be6a1e33e613c8cc5
Mirko Robbi <mirko.robbi@gmail.com>
2022-11-09 16:39:21 +0100

    cardsDinamics, need details-title banner- comments

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,16 +1,109 @@
 <script>
+import CardStructure from "../components/CardStructure.vue";
 export default {
     name: "AppContent",
     data() {
-        return {}
+        return {
+            cards: [
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+            ]
+        }
+    },
+    components: {
+        CardStructure
     }
 }
 </script>
 
 
 <template>
-    <div>
-        <h2> -- > CONTENT HERE &lt-- </h2>
+    <div class="container">
+        <div class="banner">
+            <img src="../assets/vue-dc-comics-1/img/jumbotron.jpg" alt="">
+        </div>
+
+        <div class="container-cards">
+            <div class="wrapper-cards">
+                <CardStructure v-for="(card, index) in cards" :key="index" :series="card.series" :thumb="card.thumb" />
+            </div>
+            <div class="button">
+                <button>LOAD MORE</button>
+            </div>
+        </div>
+
+
+
     </div>
 </template>
 
@@ -20,13 +113,40 @@ export default {
 @use "../styles/partials/mixins" as *;
 @use "../styles/partials/variables" as *;
 
-div {
-    height: 500px;
-    width: $container-size;
-    margin: 0 auto;
+.container {
+    width: 100%;
 
-    h2 {
-        color: rgb(63, 186, 38);
+    .banner {
+        height: 400px;
+        overflow: hidden;
+    }
+
+
+
+    .container-cards {
+        background-color: black;
+
+        .wrapper-cards {
+            width: $container-size;
+            color: white;
+            margin: 0 auto;
+            display: flex;
+            flex-wrap: wrap;
+            padding: 3rem 0;
+        }
+
+        .button {
+            @include flex-center("horizontal");
+            padding-bottom: 2rem;
+
+            button {
+                padding: .5rem 2rem;
+                background-color: #0282f9;
+                border: none;
+                color: white;
+            }
+        }
+
     }
 }
 </style>
---
+++ src/components/CardStructure.vue
@@ -0,0 +1,38 @@
+<script>
+
+export default {
+    name: "CardStructure",
+    props: {
+        thumb: String,
+        series: String
+    },
+
+
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="img-cont">
+            <img :src="thumb" :alt="series">
+        </div>
+        <h4>{{ series }}</h4>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    width: calc(100% / 6 - 10px);
+    margin: 5px;
+
+    .img-cont {
+        width: 100%;
+    }
+
+    h4 {
+        text-align: center;
+        padding: .5rem;
+    }
+
+}
+</style>
